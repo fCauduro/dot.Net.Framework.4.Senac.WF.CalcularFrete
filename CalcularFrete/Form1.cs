6b894e4c04da070f55c0868949b229891caf80aa
Fernando <fcauduro@gmail.com>
2023-10-02 21:53:21 -0300

    h

--- CalcularFrete/Form1.cs
+++ CalcularFrete/Form1.cs
@@ -16,5 +16,38 @@ namespace CalcularFrete
         {
             InitializeComponent();
         }
+
+        private void btnCalcularFrete_Click(object sender, EventArgs e)
+        {
+            var freteMinimo = Convert.ToDecimal(txtFreteMinimo.Text);
+            var uf = txtUf.Text;
+            CalcularFrete(freteMinimo, uf);
+        }
+
+
+
+
+        void CalcularFrete(decimal freteMinimo, string uf)
+        {
+            // Variavel adicional
+            var adicional = 5.0M;
+            var freteFinal = 0.0M;
+            if (uf.Equals("RS"))
+            {
+                freteFinal = freteMinimo + 0.0M;
+            }else if (uf.Equals("SC"))
+            {
+                freteFinal = freteMinimo + 1.0M;
+            }
+            else
+            {
+                freteFinal = freteMinimo + adicional;
+
+            }
+
+            txtTotalFrete.Text = freteFinal.ToString("F2");
+        }
+
+
     }
 }
